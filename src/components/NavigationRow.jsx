99be566ae93b93258da241ad128b98caced1c0f7
mreza <mrezabmiri17@gmail.com>
2023-08-16 14:27:58 +0330

    Question Page Navigators

--- src/components/NavigationRow.jsx
+++ src/components/NavigationRow.jsx
@@ -1,7 +1,7 @@
 const AnswerRow = (props) => {
   return (
     <div
-      className={`flex flex-row  h-8 w-full rounded-sm text-black items-center justify-center mt-3 `}
+      className={`flex flex-row  h-8 w-full rounded-sm text-black items-center justify-center `}
     >
       <div className="flex items-center justify-center bg-blue-300 w-1/4 h-full hover:bg-blue-700 hover:text-white transition-all duration-500 cursor-pointer mr-2">
         سوال قبل
