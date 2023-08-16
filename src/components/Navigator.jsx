a65259005f1c5aa417c242c4e888abb7086eaf46
mreza <mrezabmiri17@gmail.com>
2023-08-16 14:29:04 +0330

    Routing Completed .

--- src/components/Navigator.jsx
+++ src/components/Navigator.jsx
@@ -1,14 +1,38 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
 import { useQuery } from "react-query";
 import { fetchData } from "../api/fetchData";
 
+import Question from "../pages/Question";
+
 const Navigator = () => {
-  const { data: questions, error } = useQuery({
+  const {
+    data: questions,
+    error,
+    isLoading,
+  } = useQuery({
     queryKey: ["questions"],
     queryFn: fetchData,
   });
 
   return (
-    <div className="aspect-video w-screen h-screen bg-spiderman sm:bg-cover bg-center "></div>
+    !isLoading && (
+      <BrowserRouter>
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={<Navigate to="/question/1" replace />}
+          />
+          <Route
+            exact
+            path="/question/:id"
+            element={<Question questions={questions} />}
+          />
+          <Route path="/*" element={<Navigate to="/question/1" replace />} />
+        </Routes>
+      </BrowserRouter>
+    )
   );
 };
 
