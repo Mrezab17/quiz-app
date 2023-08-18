5f4cf5d4a01a543f40bb8824c87fcb5b0dc9803a
mreza <mrezabmiri17@gmail.com>
2023-08-19 03:21:59 +0330

    Question Component and Answer Component Synced with Store

--- src/components/AnswerRow.jsx
+++ src/components/AnswerRow.jsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
+
 const AnswerRow = (props) => {
+  const [disabled, setDisabled] = useState(props.disabled);
+  const [isCorrect, setIsCorrect] = useState(props.isCorrect);
+  const [isSelected, setIsSelected] = useState(props.isSelected);
+
+  useEffect(() => {
+    setIsCorrect(props.isCorrect);
+    setIsSelected(props.isSelected);
+    setDisabled(props.disabled);
+  }, [props]);
   return (
     <div
-      className={`flex flex-row bg-black h-10 w-full rounded-sm text-white items-center px-2 hover:bg-gray-100 hover:text-black transition-all duration-500 cursor-pointer`}
+      className={`flex flex-row  h-10 w-full rounded-sm items-center bg-black text-white  px-2 ${
+        disabled
+          ? ""
+          : "hover:bg-gray-100 hover:text-black transition-all duration-500 cursor-pointer"
+      }
+      ${isCorrect && disabled ? "bg-green-600" : ""}
+      ${!isCorrect && isSelected ? "bg-red-600" : ""}
+      `}
+      onClick={disabled ? () => {} : props.onAnswer}
     >
       {props.children}
     </div>
--- src/pages/Question.jsx
+++ src/pages/Question.jsx
@@ -1,25 +1,47 @@
 import { useParams } from "react-router-dom";
+
 import QuestionRow from "../components/QuestionRow";
 import AnswerRow from "../components/AnswerRow";
 import NavigationRow from "../components/NavigationRow";
+import ResetButton from "../components/ResetButton";
+
+import { useDispatch, useSelector } from "react-redux";
+import { setAnswer } from "../store/answeredQuestionsSlice";
 
 const Question = (props) => {
   const { id } = useParams();
+  const dispatch = useDispatch();
+  const currentAnswer = useSelector(
+    (state) => state.answeredQuestions.items[id - 1]
+  );
+
   const currentQuestion = props.questions[id - 1];
+  const currentCorrect = currentQuestion.correct;
   const question = currentQuestion.question;
   const options = currentQuestion.options;
-  const picName = currentQuestion.picName;
+
   return (
     <div
-      className={`aspect-video w-screen h-screen bg-[url('../assets/ironman.jpg')] bg-cover sm:bg-cover bg-right flex items-center justify-center`}
+      className={`aspect-video w-screen h-screen bg-[url('../assets/ironman.jpg')] bg-cover sm:bg-cover bg-right flex flex-col space-y-3 items-center justify-center`}
     >
       <div className="h-1/2 w-7/12 bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-primary flex flex-col px-3 space-y-2.5 pt-2">
         <QuestionRow>{question}</QuestionRow>
         {options.map((option, index) => (
-          <AnswerRow key={id * 4 + index}>{option}</AnswerRow>
+          <AnswerRow
+            onAnswer={() => {
+              dispatch(setAnswer({ number: id - 1, answer: index + 1 }));
+            }}
+            key={id * 4 + index}
+            disabled={parseInt(currentAnswer) !== 0}
+            isSelected={parseInt(currentAnswer) === index + 1}
+            isCorrect={parseInt(currentCorrect) === index + 1}
+          >
+            {option}
+          </AnswerRow>
         ))}
         <NavigationRow currentPage={id} />
       </div>
+      <ResetButton />
     </div>
   );
 };
