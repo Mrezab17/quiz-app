43c4dd034bbb3b9f828c3d9787e810d8ee5275f3
mreza <mrezabmiri17@gmail.com>
2023-08-21 16:43:33 +0330

    Added Question Number Label To QuestionRow

--- src/components/QuestionRow.jsx
+++ src/components/QuestionRow.jsx
@@ -1,7 +1,12 @@
 const QuestionRow = (props) => {
   return (
-    <div className={`flex flex-row  h-20 w-full text-white items-center px-2`}>
-      {props.children}
+    <div className={`flex flex-row h-16  px-2`}>
+      <div className="h-full w-10/12 text-white items-center">
+        {props.children}
+      </div>
+      <div
+        className={`text-left w-2/12 text-blue-500  `}
+      >{`       ${props.number}/10`}</div>
     </div>
   );
 };
