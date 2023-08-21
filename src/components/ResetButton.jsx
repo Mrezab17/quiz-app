091dbe21d282918b192c619d6653ce6ab55b10a2
mreza <mrezabmiri17@gmail.com>
2023-08-21 10:39:32 +0330

    ResetButton Test Added .

--- src/components/ResetButton.jsx
+++ src/components/ResetButton.jsx
@@ -4,14 +4,14 @@ import { resetAnswers } from "../store/answeredQuestionsSlice";
 const ResetButton = () => {
   const dispatch = useDispatch();
   return (
-    <div
+    <button
       className="h-14 w-40 cursor-pointer text-white text-xl  bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-primary flex flex-col items-center justify-center "
       onClick={() => {
         dispatch(resetAnswers());
       }}
     >
       شروع دوباره
-    </div>
+    </button>
   );
 };
 
